2f8dbc1f916ba0062b0ca25e26300a95813bfbdf
Dravik Italiya <dravikitaliya7@gmail.com>
2025-05-30 21:45:28 +0530

    soul-scribe: lib functions for server

---
+++ server/lib/db.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config'; 
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "@shared/schema";
+
+if (!process.env.VITE_DATABASE_URL) throw new Error("DATABASE_URL is not defined");
+
+const client = postgres(process.env.VITE_DATABASE_URL);
+export const db = drizzle(client, { schema });
